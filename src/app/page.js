f72da5575c0906f071225a2ab6f4e61d9ae05c7c
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-08-30 22:09:34 +0200

    add base headers

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import DarkModeButton from "@/app/components/DarkModeButton";
 export default function Home() {
     return (
         <main>
-            <h1 className="text-4xl">Test</h1>
+            <h1>Test</h1>
             <DarkModeButton />
         </main>
     );
