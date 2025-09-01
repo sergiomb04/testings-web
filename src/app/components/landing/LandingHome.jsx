0d26332933d9f55b18f38d70059b3d770b369632
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-09-01 02:46:43 +0200

    TEXT SIZE

--- src/app/components/landing/LandingHome.jsx
+++ src/app/components/landing/LandingHome.jsx
@@ -5,7 +5,7 @@ export default function LandingHome() {
         <div className="flex justify-center align-middle items-center my-30">
             <div className="flex flex-col justify-center text-center w-1/2 gap-8">
                 <h1 className="font-extrabold">Modern Landing Page</h1>
-                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                <h2 className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
                     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
