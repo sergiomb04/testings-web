5bae4f6e0886b62034eef88745acd4cc6b156ab2
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-09-01 02:27:37 +0200

    landing mejora botón

--- src/app/components/landing/GetStartedButton.jsx
+++ src/app/components/landing/GetStartedButton.jsx
@@ -2,8 +2,8 @@
 
 export default function GetStartedButton() {
     return (
-        <a href="/work"
-        className="rounded-full border-gray-600 p-6 border-1 w-max px-18 m-auto
+        <a href="/contact"
+        className="text-xl font-semibold rounded-full border-gray-600 p-6 border-1 w-max px-18 m-auto
         transform transition-transform duration-200 hover:scale-105">
             <button
                 className=" cursor-pointer">
