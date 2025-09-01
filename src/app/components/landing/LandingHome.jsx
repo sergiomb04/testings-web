58bd49344ed30cfaf7b614de86b236b75f537350
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-09-01 02:25:15 +0200

    landing title: bold

--- src/app/components/landing/LandingHome.jsx
+++ src/app/components/landing/LandingHome.jsx
@@ -4,7 +4,7 @@ export default function LandingHome() {
     return (
         <div className="flex justify-center align-middle items-center my-30">
             <div className="flex flex-col justify-center text-center w-1/2 gap-8">
-                <h1>Modern Landing Page</h1>
+                <h1 className="font-extrabold">Modern Landing Page</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
