0efcfd56ff92d676c703d2f2bde300d3463574e1
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-09-01 18:26:23 +0200

    mejora + responsive

--- src/app/items/ObjectsComp.jsx
+++ src/app/items/ObjectsComp.jsx
@@ -1,24 +1,20 @@
-import Image from "next/image";
-
 export default async function ItemsComponent() {
     const res = await fetch("https://fakestoreapi.com/products");
     const data = await res.json();
 
-    const imageStyle = {
-        width: '100px',
-        height: 'auto',
-    }
-
     return (
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 xl:grid-cols-4 md:grid-cols-2 gap-4">
             {data.map((item, index) => (
-                <div key={index}
-                className={"border-1 border-gray-600 p-4 rounded-2xl"}>
-                    <div className={"flex m-auto min-w-max justify-center pb-4"}>
-                        <Image src={item.image} alt={"img"} width={100} height={100} style={imageStyle}/>
+                <div key={index} className="flex flex-col border border-gray-600 p-4 rounded-2xl">
+                    <div className="flex justify-center items-center h-32 mb-4">
+                        <img
+                            src={item.image}
+                            alt="img"
+                            className="max-h-full w-auto object-contain"
+                        />
                     </div>
                     <h1 className="text-2xl font-semibold">{item.title}</h1>
-                    <p className={"text-xl pt-2 line-clamp-2 overflow-hidden"}>{item.description}</p>
+                    <p className="text-xl pt-2 line-clamp-2 overflow-hidden">{item.description}</p>
                 </div>
             ))}
         </div>
