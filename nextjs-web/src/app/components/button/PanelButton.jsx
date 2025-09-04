d7f24dbcaa346e676e1dfda821d10b30a563f9ef
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-09-04 22:14:09 +0200

    starting panel components

---
+++ nextjs-web/src/app/components/button/PanelButton.jsx
@@ -0,0 +1,7 @@
+export default function PanelButton() {
+    return (
+        <a className={"bg-stone-950 hover:bg-stone-800 p-2 rounded-sm border-1 border-stone-600 cursor-pointer"}>
+            <span className={"px-4"}>Test</span>
+        </a>
+    )
+}
--- nextjs-web/src/app/testing/page.js
+++ nextjs-web/src/app/testing/page.js
@@ -1,9 +1,12 @@
+import PanelButton from "@/app/components/button/PanelButton";
+
 export default function TestingPage() {
 
     return (
         <div>
             <h1 className={"font-bold pb-8"}>Testing</h1>
             {/*<ChatComponent/>*/}
+            <PanelButton />
         </div>
     )
 }
