c466f30cb98efb339eb9bd73fef94aad9332f162
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-09-08 16:40:14 +0200

    OSTIA - upload gradle things

---
+++ testings-web-backend/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("java")
+    id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "me.imsergioh.testingsweb"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("jakarta.websocket:jakarta.websocket-api:2.1.1")
+
+    implementation ("org.glassfish.tyrus:tyrus-server:2.1.1")
+    implementation ("org.glassfish.tyrus:tyrus-container-grizzly-server:2.1.1")
+
+    implementation ("org.glassfish.tyrus:tyrus-client:2.1.1")
+
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    testCompileOnly("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    implementation("org.mongodb:bson:5.5.1")
+}
+
+application {
+    mainClass.set("me.imsergioh.testingsweb.MyWebSocketServer")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("server")   // nombre base
+    archiveVersion.set("")          // vacío = sin versión en el nombre
+    archiveClassifier.set("")       // vacío = sin "-all"
+}
+
---
+++ testings-web-backend/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "testings-web-backend"
+
