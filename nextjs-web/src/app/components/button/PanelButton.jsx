441cfda7b800a9db49ef7ba698622bf1ac843989
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-09-08 13:21:17 +0200

    add onClick

--- nextjs-web/src/app/components/button/PanelButton.jsx
+++ nextjs-web/src/app/components/button/PanelButton.jsx
@@ -1,9 +1,11 @@
-export default function PanelButton({displayText, small = false}) {
+export default function PanelButton({displayText, small = false, onClick}) {
 
     const paddingYSize = small ? 'py-1.25' : 'py-2.5';
 
     return (
-        <a className={`bg-neutral-900 hover:bg-neutral-800 border-neutral-600 border-1 ${paddingYSize} rounded-md cursor-pointer transition-colors duration-150`}>
+        <a
+            onClick={onClick ? onClick : undefined}
+            className={`bg-neutral-900 hover:bg-neutral-800 border-neutral-600 border-1 ${paddingYSize} rounded-md cursor-pointer transition-colors duration-150`}>
             <span className={"px-2 font-semibold text-sm"}>{displayText}</span>
         </a>
     )
