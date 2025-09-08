5ab69c9a3393fa54d5ca154867f8a04d6a317698
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-09-08 13:21:37 +0200

    change to use react-hot-toast

--- nextjs-web/src/app/testing/page.js
+++ nextjs-web/src/app/testing/page.js
@@ -2,39 +2,29 @@
 
 import wsClient from "@/lib/wsClient";
 import {useEffect, useState} from "react";
+import PanelButton from "@/app/components/button/PanelButton";
+import {toast, Toaster} from "react-hot-toast";
 
 export default function TestingPage() {
-
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         wsClient.connect("ws://localhost:8080/ws");
 
         const onEvent = (data) => console.log("📥 Evento completo:", data);
         const onResponse = (payload) => console.log("📥 Respuesta:", payload.message);
-        const onError = (payload) => {
-            console.log("❌ Error:", payload.message);
-            setError(payload.message)
-        }
+        const onError = (payload) => toast.error(payload.message);
         const onCommand = (cmd) => console.log("📥 Comando:", cmd.label);
-        const onOpen = () => {
-            wsClient.sendCommand("test arg1 arg2 arg3");
-            //wsClient.sendEvent("RESPONSE", { message: "Test log en plan fak" });
-        };
 
         wsClient.on("event", onEvent);
         wsClient.on("RESPONSE", onResponse);
         wsClient.on("ERROR", onError);
         wsClient.on("command", onCommand);
-        wsClient.on("open", onOpen);
 
         return () => {
             wsClient.off("event", onEvent);
             wsClient.off("RESPONSE", onResponse);
             wsClient.off("ERROR", onResponse);
             wsClient.off("command", onCommand);
-            wsClient.off("open", onOpen);
-
             wsClient.disconnect();
         };
     }, []);
@@ -43,11 +33,12 @@ export default function TestingPage() {
         <div>
             <h1 className={"font-bold pb-8"}>Testing</h1>
 
-            {error && (
-                <div className="p-4 rounded-2xl bg-red-100 text-red-700 shadow-md mb-4">
-                    <strong>❌ Error:</strong> {error}
-                </div>
-            )}
+            <Toaster />
+
+            <div>
+                <PanelButton displayText={"Test tarea"} onClick={() => wsClient.sendCommand("test arg1 arg2 arg3")}/>
+            </div>
+
         </div>
     )
 }
