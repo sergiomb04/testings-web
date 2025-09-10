c8597553267420f73325334d509817d1b7ad2e86
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-09-10 16:07:21 +0200

    fix gradle - mongodb functional

--- testings-web-backend/src/main/java/me/imsergioh/testingsweb/connection/MongoDBConnection.java
+++ testings-web-backend/src/main/java/me/imsergioh/testingsweb/connection/MongoDBConnection.java
@@ -1,2 +1,27 @@
-package me.imsergioh.testingsweb.connection;public class MongoDBConnection {
+package me.imsergioh.testingsweb.connection;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import lombok.Getter;
+import org.bson.Document;
+
+@Getter
+public class MongoDBConnection {
+
+    private final MongoClient client;
+
+    public MongoDBConnection(String uri) {
+        client = MongoClients.create(uri);
+    }
+
+    public MongoDatabase getDatabase(String name) {
+        return client.getDatabase(name);
+    }
+
+    public MongoCollection<Document> getCollection(String database, String collection) {
+        return client.getDatabase(database).getCollection(collection);
+    }
+
 }
