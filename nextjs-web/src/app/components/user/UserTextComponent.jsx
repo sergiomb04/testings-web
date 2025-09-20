279f7ece17d910db8240772352083c125a057fa6
ImSergioh <50914599+sergiomb04@users.noreply.github.com>
2025-09-21 01:40:32 +0200

    additional text: Working

--- nextjs-web/src/app/components/user/UserTextComponent.jsx
+++ nextjs-web/src/app/components/user/UserTextComponent.jsx
@@ -1,14 +1,30 @@
 import {cookies} from "next/headers";
 import {getValid} from "@/lib/AuthController";
 
-async function getUserData(username) {
+async function getUserData() {
     const cookieStore = await cookies();
     const token = cookieStore.get("token")?.value || "";
+    if (!token) return null;
     const valid = await getValid(token);
+    if (!valid) return null;
+
+    const res = await fetch("http://localhost:8080/api/user/admin", {
+        method: "GET",
+        headers: {
+            Cookie: `token=${token}`,
+        },
+        cache: "no-store"
+    });
+
+    if (!res.ok) return null;
+    const user = await res.json();
+    // filtra lo que NO quieres exponer
+    return { id: user._id, username: user.username, role: user.role, text: user?.text };
 }
 
-export default async function UserTextComponent({username}) {
-    const userData = await getUserData(username);
+export default async function UserTextComponent() {
+    const userData = await getUserData();
+
     const text = userData.text;
 
     return (text ?
--- nextjs-web/src/app/page.js
+++ nextjs-web/src/app/page.js
@@ -1,9 +1,11 @@
 import LandingHome from "@/app/components/landing/LandingHome";
+import UserTextComponent from "@/app/components/user/UserTextComponent";
 
 export default function Home() {
     return (
         <div>
             <LandingHome />
+            <UserTextComponent />
         </div>
     );
 }
